95c84d0773ec2e5b25751a9948412a52c9aaa8ac
Alex Yang <71572454+AZYDEVE@users.noreply.github.com>
2021-12-12 04:55:04 +0800

    final changes

--- front/src/utils/webSocket.js
+++ front/src/utils/webSocket.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const OpenSocket = () => {
-  const ws = new WebSocket("ws://localhost:3000");
+  const ws = new WebSocket("ws://localhost:3004");
 
   ws.onopen = function (event) {
     console.log("socket is connected");
